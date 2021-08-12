b441a286fee73d61a4b415ff6ab7de33d2a4595f
Braden Wilson <brwilson@redhat.com>
2021-08-12 09:05:37 -0700

    Formatted server/index with prettier

--- util/server/index.ts
+++ util/server/index.ts
@@ -3,15 +3,12 @@ import path from 'path';
 
 import type { TitleDescriptionList } from 'custom-types';
 
-export const getStartingFile = async (
-  fileRelPath: string
-): Promise<string> => {
+export const getStartingFile = async (fileRelPath: string): Promise<string> => {
   const splitRelFilePath = fileRelPath.split('/');
   const absFilePath = path.join(process.cwd(), ...splitRelFilePath);
   const startingFile = await fs.readFile(absFilePath, 'utf8');
   return startingFile;
-
-}
+};
 
 export const getTitleDescriptionList = async (
   fileRelPath: string
