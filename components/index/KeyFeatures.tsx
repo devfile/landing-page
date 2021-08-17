6a082bb26c960263ea4497fee04a6d8b9bf2f01a
Braden Wilson <brwilson@redhat.com>
2021-08-17 11:00:14 -0700

    Key features now align center

--- components/index/KeyFeatures.tsx
+++ components/index/KeyFeatures.tsx
@@ -16,27 +16,29 @@ export interface KeyFeaturesProps {
 }
 
 const KeyFeatures: React.FC<KeyFeaturesProps> = ({ keyFeaturesList }: KeyFeaturesProps) => (
-  <div style={{ maxWidth: '1250px' }}>
-    <h1 style={{ fontSize: '2rem' }}>Key Features</h1>
-    <h4 style={{ color: 'var(--pf-global--Color--200)' }}>with devfiles</h4>
-    <br />
-    <Gallery hasGutter>
-      {Object.entries(keyFeaturesList as TitleDescriptionList).map(([title, description]) => (
-        <Card key={title}>
-          <CardHeader>
-            <CardHeaderMain>
-              <Brand
-                src={cogIcon}
-                alt="Placeholder"
-                style={{ height: '5rem', marginLeft: '40%' }}
-              />
-            </CardHeaderMain>
-          </CardHeader>
-          <CardTitle>{title}</CardTitle>
-          <CardBody>{description}</CardBody>
-        </Card>
-      ))}
-    </Gallery>
+  <div style={{ justifyContent: 'center', display: 'flex', width: '100%' }}>
+    <div style={{ maxWidth: '1250px' }}>
+      <h1 style={{ fontSize: '2rem' }}>Key Features</h1>
+      <h4 style={{ color: 'var(--pf-global--Color--200)' }}>with devfiles</h4>
+      <br />
+      <Gallery hasGutter>
+        {Object.entries(keyFeaturesList as TitleDescriptionList).map(([title, description]) => (
+          <Card key={title}>
+            <CardHeader>
+              <CardHeaderMain>
+                <Brand
+                  src={cogIcon}
+                  alt="Placeholder"
+                  style={{ height: '5rem', marginLeft: '40%' }}
+                />
+              </CardHeaderMain>
+            </CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardBody>{description}</CardBody>
+          </Card>
+        ))}
+      </Gallery>
+    </div>
   </div>
 );
 
