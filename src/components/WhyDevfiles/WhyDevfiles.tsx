817f7448a70392ee5fe831574bab488fa867ba36
Michael Valdron <mvaldron@redhat.com>
2022-05-26 17:33:18 -0400

    demo now shows as static image when viewed on mobile device.
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- src/components/WhyDevfiles/WhyDevfiles.tsx
+++ src/components/WhyDevfiles/WhyDevfiles.tsx
@@ -1,6 +1,7 @@
 import { TextContainer } from 'custom-types';
 import { Text, TextContent, TextVariants } from '@patternfly/react-core';
 import { GifPlayer } from '@src/components';
+import { useWindowDimensions } from '@src/util/client';
 import styles from './WhyDevfiles.module.css';
 
 export interface WhyDevfilesProps {
@@ -8,7 +9,13 @@ export interface WhyDevfilesProps {
 }
 
 export const WhyDevfiles: React.FC<WhyDevfilesProps> = (props: WhyDevfilesProps) => {
+  const minDemoSize = 480;
   const { whyDevfiles } = props;
+  const { height, width } = useWindowDimensions();
+  const demoFile =
+    width !== undefined && height !== undefined && Math.min(height, width) < minDemoSize
+      ? '/images/demo.png'
+      : '/images/demo.gif';
 
   return (
     <div className={styles.container}>
@@ -19,7 +26,7 @@ export const WhyDevfiles: React.FC<WhyDevfilesProps> = (props: WhyDevfilesProps)
         <Text className={styles.bodyText}>{whyDevfiles.body}</Text>
       </TextContent>
       <br />
-      <GifPlayer width="1920px" height="1080px" src="/images/demo.gif" />
+      <GifPlayer width="1920px" height="1080px" src={demoFile} />
     </div>
   );
 };
